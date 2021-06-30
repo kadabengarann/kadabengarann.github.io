25497af64e8fcde5482afd61ee759d9736bcd895
Ardhy Satrio <rio27dh@gmail.com>
2021-07-01 01:34:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function your_func(){
 }
 document.addEventListener("DOMContentLoaded", event =>{
     setTimeout(function() { your_func(); }, 2500);
-
+window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ?autoplay=1";
     const burger = document.querySelector('.menu_burger');
     const bar1 = document.querySelector('.menu_burger-atas');
     const bar2 = document.querySelector('.menu_burger-bwh');
