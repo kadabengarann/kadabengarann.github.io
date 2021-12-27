2c024d35e9dbea4ea22b4d90e0e35b1c094faf27
kadabengarann <rio27dh@gmail.com>
2021-12-27 15:43:26 +0800

    test all project list

--- src/components/ProjectPage.js
+++ src/components/ProjectPage.js
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { useParams } from "react-router-dom";
+
 import Footer from "./Footer";
-import "../style/project.scss";
+import "../style/projects.scss";
 
 const projects = require("./projects");
 
@@ -11,17 +11,61 @@ const ProjectPage = () => {
   const { projectId } = useParams();
   let image = "../img/bg-jumbo-5.jpg";
   return (
-    <>
-            <p style={{color: "red"}}>
-        <strong>Project ID: </strong>
-        {projectId}
-      </p>
-      <p style={{color: "red"}}>
-        <strong>Project Name: </strong>
-        {projects[projectId - 1].name}
-      </p>
+              <><section className="section2" data-scroll-section id="data-sec2">
+        <div className="content_section" data-scroll data-scroll-speed="0">
 
-    </>
+              <div className="projects_section" id="direction">
+            <h1 className="section-title projects" data-scroll-call="testEvent1" data-scroll-repeat="true" data-scroll
+              data-scroll-offset="-1000">
+              <div className="section-title-container" data-scroll data-scroll-direction="horizontal"
+                data-scroll-speed="1" data-scroll-target="#direction">
+                <div className="section-title-inner">
+                  <span className="section-title-inner-text">{projectId}</span>
+                </div>
+                <div className="section-title-inner">
+                  <span className="section-title-inner-text">Projects</span>
+                </div>
+                <div className="section-title-inner">
+                  <span className="section-title-inner-text">Projects</span>
+                </div>
+              </div>
+            </h1>
+            {projects.map((project, index) => (
+              <div key={index}><section className="block_details">
+                <div className="block_details_inner">
+                  <div className="block_details_text" data-scroll-repeat="false" data-scroll data-scroll-offset="200">
+                    <h1 className="block_details_title" >{project.name}</h1>
+                    <p>
+                      {project.desc}
+                    </p>
+                    <div className="block_details_links" data-scroll>
+                    <a href={project.demo} target="_blank"
+                    className="o-link">
+                    Visit website
+                  </a>
+                    </div>
+                  </div>
+                  <div className="block_details_photo" data-scroll data-scroll-offset="200">
+                    <div className="base">
+                      <div className="hole">
+                          <a href={`/project/`+(index+1)} 
+                    className="o-link">
+                    View details
+                  </a>
+                      </div>
+                    </div>
+                    <div style={{   
+  backgroundImage: "url(" + project.thumb + ")" }} className="back" data-scroll data-scroll-speed="-0.8"></div>
+                  </div>
+                </div>
+              </section></div>
+            ))}
+            <a className="anchor" id="contact" data-scroll></a>
+          </div>
+          <Footer/>
+        </div>
+      </section></> 
+
   );
 };
 
