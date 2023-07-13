07790812d88e65fd27450807e6033a8252cd6f31
Ardhy Satrio <rio27dh@gmail.com>
2023-07-13 23:53:33 +0800

    change tld

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
             <ul className="navbar_pages hide">
                 <li className="navbar_page"><a className="navbar_pages_link a-move" href="/">Home</a></li>
                 <li className="navbar_page"><a className="navbar_pages_link a-move" href="/projects">Projects</a></li>
-                <li className="navbar_page"><a className="navbar_pages_link a-move" href="https://blog.ardhysatrio.works/" target="_blank">Blog</a></li>
+                <li className="navbar_page"><a className="navbar_pages_link a-move" href="https://blog.ardhysatrio.tech/" target="_blank">Blog</a></li>
                 <li className="navbar_page"><i className="navbar_pages_link a-move" href="#" disabled style={{ fontStyle: 'normal' }}>Info</i></li>
             </ul>
             <ul className="navbar_links hide">
